637ddcc5482211f4c45d12fbc9366cc9d40cff56
nealith <nealith@neaworld.fr>
2018-02-08 15:41:59 +0100

    fix 'fake scroll' , add scroll on mouse movement on Y axis

--- montageShutter.js
+++ montageShutter.js
@@ -23,6 +23,7 @@ montageShutter = new Vue({
   data:{
     div:document.getElementById('montageShutter'),
     montages:montages,
+    down:false,
     style:{
       height : frame.h+'px',
       background : 'orange',
@@ -31,9 +32,6 @@ montageShutter = new Vue({
       left : (sizePourcent*frame.w*2)+'px',
       width : (frame.w - sizePourcent*frame.w*3)+'px',
       overflow : 'hidden'
-    },
-    styleMontages:{
-      width : (frame.w - sizePourcent*frame.w*3)+'px'
     },
     styleMontage:{
       height : size+'px',
@@ -48,15 +46,13 @@ montageShutter = new Vue({
       this.style.height = frame.h+'px';
       this.style.width = (strToFloat(libraryStrip.style.left)-(strToFloat(montageStrip.style.width)+strToFloat(montageStrip.style.left))) +'px';
 
-      this.styleMontages = (strToFloat(libraryStrip.style.left)-(strToFloat(montageStrip.style.width)+strToFloat(montageStrip.style.left))) + 'px';
-
       size = Math.min(frame.h*0.8,(strToFloat(libraryStrip.style.left)-(strToFloat(montageStrip.style.width)+strToFloat(montageStrip.style.left)))*0.8);
 
       this.styleMontage.height = size+'px';
       this.styleMontage.width = size+'px';
 
-      this.styleMontages['padding-left'] = 'auto';
-      this.styleMontages['padding-right'] = 'auto';
+      this.style['padding-left'] = 'auto';
+      this.style['padding-right'] = 'auto';
 
       this.styleMontage.margin = size/8.0+'px auto '+size/8.0+'px auto';
 
@@ -66,14 +62,40 @@ montageShutter = new Vue({
     onWheel:function(e){
       var delta = Math.max(-1, Math.min(1, e.deltaY));
       document.getElementById(e.currentTarget.id).scrollTop += (delta*40); // Multiplied by 40
+      var t = document.getElementById(e.currentTarget.id);
+
       e.preventDefault();
+    },
+    mouseDown:function(e){
+      //DEBUG
+      //console.log('libraryStrip:mouseDown');
+      this.down = true;
+    },
+    mouseUp:function(e){
+      //DEBUG
+      //console.log('libraryStrip:mouseUp');
+      this.down = false;
+    },
+    mouseMove:function(e){
+      //DEBUG
+      //console.log('libraryStrip:mouseMove');
+      if (this.down == true) {
+        var delta = Math.max(-1, Math.min(1, e.movementY));
+        console.log(e.movementY);
+        document.getElementById(e.currentTarget.id).scrollTop -= (delta*40); // Multiplied by 40
+        var t = document.getElementById(e.currentTarget.id);
+        e.preventDefault();
+        //DEBUG
+        //console.log('libraryStrip:mouseMove:true');
+
+      }
     }
   }
 });
 
-montageShutter.styleMontages['padding-left'] = 'auto';
-montageShutter.styleMontages['padding-right'] = 'auto';
-montageShutter.styleMontages['overflow-y'] = 'scroll';
-montageShutter.styleMontages['overflow-x'] = 'hidden';
-montageShutter.styleMontages['margin-right'] = '-14px';
-montageShutter.styleMontages['white-space'] = 'nowrap';
+montageShutter.style['padding-left'] = 'auto';
+montageShutter.style['padding-right'] = 'auto';
+montageShutter.style['overflow-y'] = 'scroll';
+montageShutter.style['overflow-x'] = 'hidden';
+montageShutter.style['margin-right'] = '-14px';
+montageShutter.style['white-space'] = 'nowrap';
