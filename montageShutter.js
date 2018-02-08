fd1ecc2fc31f9cdf5ed10c62dbd803cc2eec9197
nealith <nealith@neaworld.fr>
2018-02-08 14:41:01 +0100

    Add montage scrolling

--- montageShutter.js
+++ montageShutter.js
@@ -1,14 +1,45 @@
+var montages = [
+  {
+    name:"montage1"
+  },
+  {
+    name:"montage2"
+  },
+  {
+    name:"montage3"
+  },
+  {
+    name:"montage4"
+  },
+  {
+    name:"montage5"
+  }
+]
+
+var size = Math.min(frame.h*0.8,(strToFloat(libraryStrip.style.left)-(strToFloat(montageStrip.style.width)+strToFloat(montageStrip.style.left)))*0.8);
+
 montageShutter = new Vue({
   el: '#montageShutter',
   data:{
     div:document.getElementById('montageShutter'),
+    montages:montages,
     style:{
       height : frame.h+'px',
       background : 'orange',
       position : 'absolute',
       top : '0px',
       left : (sizePourcent*frame.w*2)+'px',
+      width : (frame.w - sizePourcent*frame.w*3)+'px',
+      overflow : 'hidden'
+    },
+    styleMontages:{
       width : (frame.w - sizePourcent*frame.w*3)+'px'
+    },
+    styleMontage:{
+      height : size+'px',
+      background : 'pink',
+      width : size+'px',
+      margin : size/8.0+'px auto '+size/8.0+'px auto'
     }
   },
   methods:{
@@ -17,13 +48,27 @@ montageShutter = new Vue({
       this.style.height = frame.h+'px';
       this.style.width = (strToFloat(libraryStrip.style.left)-(strToFloat(montageStrip.style.width)+strToFloat(montageStrip.style.left))) +'px';
 
-    }    ,
-    onScroll:function(){
-      console.log("scroll");
-      //console.log(e.id);
-      //var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-      //document.getElementById('yourDiv').scrollLeft -= (delta*40); // Multiplied by 40
+      this.styleMontages = (strToFloat(libraryStrip.style.left)-(strToFloat(montageStrip.style.width)+strToFloat(montageStrip.style.left))) + 'px';
+
+      size = Math.min(frame.h*0.8,(strToFloat(libraryStrip.style.left)-(strToFloat(montageStrip.style.width)+strToFloat(montageStrip.style.left)))*0.8);
+
+      this.styleMontage.height = size+'px';
+      this.styleMontage.width = size+'px';
+
+
+
+    },
+    onWheel:function(e){
+      var delta = Math.max(-1, Math.min(1, e.deltaY));
+      document.getElementById(e.currentTarget.id).scrollTop += (delta*40); // Multiplied by 40
       e.preventDefault();
     }
   }
 });
+
+montageShutter.styleMontage['padding-left'] = 'auto';
+montageShutter.styleMontage['padding-right'] = 'auto';
+montageShutter.styleMontage['overflow-y'] = 'scroll';
+montageShutter.styleMontage['overflow-x'] = 'hidden';
+montageShutter.styleMontage['margin-right'] = '-14px';
+montageShutter.styleMontage['white-space'] = 'nowrap';
